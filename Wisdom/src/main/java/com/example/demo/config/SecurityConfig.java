aa73f587d81a906a91ca388d4837431fe88456b2
BioCode67 <6wngud@gmail.com>
2025-12-29 19:51:45 +0900

    Fix: Force update SecurityConfig

--- Wisdom/src/main/java/com/example/demo/config/SecurityConfig.java
+++ Wisdom/src/main/java/com/example/demo/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.example.demo.config;
+package com.example.demo.config; //commit test
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
